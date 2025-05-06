c544cdb4346a94725c9d6815d66ff76a80b2a789
Sefa DİNÇER <sefadincer1@icloud.com>
2025-05-06 15:27:01 +0300

    Changed app name

--- silivri_havuz/android/app/src/main/kotlin/com/silivribel/havuz/silivri_havuz/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.silivribel.havuz.silivri_havuz
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- silivri_havuz/lib/model/home_screen_model.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class HomeScreenModel {
-  HomeScreenModel({required this.title, required this.icon, this.body = const SizedBox()});
-  IconData icon;
-  String title;
-  Widget body;
-}
